02017917f39e2a70c1669a912a66c219308c28f4
Oscar Chavoya Aceves <chavoyao@gmail.com>
2019-01-06 18:03:55 -0700

    updated authentication services

--- src/app/services/rest-client.service.ts
+++ src/app/services/rest-client.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError} from 'rxjs';
 import { ItemDTO, WhiteBoardItem, RegistrationDTO, LoginDTO, RestMessage } from '../model/whiteboard';
-import { HttpClient, HttpHandler } from '@angular/common/http';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 
 export class RestClientService {
 
   api='http://localhost:8080/whiteboard';
+
+  getHeaders(): HttpHeaders {
+   let headers = new HttpHeaders();
+   headers.set('Content-Type','application/JSON')
+   return headers;
+  }
 
   constructor(private http: HttpClient, private httpHandler: HttpHandler) { }
 
@@ -16,11 +22,15 @@ export class RestClientService {
   }
 
   login(loginDTO: LoginDTO): Observable<RestMessage<string>> {
-    return this.restClient('post', '/register', loginDTO) as Observable<RestMessage<string>>;
+    return this.restClient('post', '/login', loginDTO) as Observable<RestMessage<string>>;
+  }
+
+  logout(username: string): Observable<RestMessage<string>> {
+    return this.restClient('post', '/login', username) as Observable<RestMessage<string>>;
   }
 
   create(itemDTO: ItemDTO): Observable<RestMessage<ItemDTO>> {
-    return this.restClient('post', '/register', itemDTO) as Observable<RestMessage<ItemDTO>>;
+    return this.restClient('post', '/create', itemDTO) as Observable<RestMessage<ItemDTO>>;
   }
 
   load(): Observable<RestMessage<ItemDTO[]>> {
@@ -32,10 +42,10 @@ export class RestClientService {
   
     switch (method) {
       case 'get':
-        return this.http.get(path) as Observable<RestMessage<any>>;
+        return this.http.get(path, {headers:this.getHeaders()}) as Observable<RestMessage<any>>;
       default:
         if (data == null) throwError('POST without data');
-        return this.http.post(path, data) as Observable<RestMessage<any>>;
+        return this.http.post(path, data, {headers:this.getHeaders()}) as Observable<RestMessage<any>>;
     }
   }
 }
