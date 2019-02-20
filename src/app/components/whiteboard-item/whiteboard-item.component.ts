b1b954bd6bea6c43aaf0cc61a1576960862df69f
Oscar Chavoya Aceves <chavoyao@gmail.com>
2019-02-20 07:46:25 -0700

    Updated white-board-detial style

--- src/app/components/whiteboard-item/whiteboard-item.component.ts
+++ src/app/components/whiteboard-item/whiteboard-item.component.ts
@@ -4,14 +4,13 @@ import { Timestamp } from 'rxjs/internal/operators/timestamp';
 
 @Component({
   selector: 'app-whiteboard-item',
-  template: `<p (click)="toggleDetail()">{{ item.title }}</p>
-  <div *ngIf="detailVisible" class="whiteboard-detail">
-    {{ item.detail }}<br />
+  template: `
+  <span (click)="toggleDetail()">{{ item.title }}</span><br>
+  <span *ngIf="detailVisible" class="whiteboard-detail">{{ item.detail }}</span><br>
     <span class="whiteboard-detail-expires">expires on: {{ item.expiresOn | date: 'dd/MM/yyyy' }}</span>
-  </div>
   `,
-  styles: ['.whiteboard-detail={ font-size: small}',
-           '.whiteboard-detail-expires={ font-size: xx-small}']
+  styles: ['.whiteboard-detail={ font-size: small; }',
+           '.whiteboard-detail-expires={ font-size: xx-small; color:red; }']
 })
 export class WhiteboardItemComponent implements OnInit {
   @Input()
