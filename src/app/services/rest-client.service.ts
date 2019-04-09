a045f64acc0d007661e72614eff0a5f45905907b
Chavoya-Aceves <chavoyao@gmail.com>
2019-04-09 07:59:58 -0700

    Changed observables to subjects

--- src/app/services/rest-client.service.ts
+++ src/app/services/rest-client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, Subject } from 'rxjs';
 import {
   ItemDTO,
   RegistrationDTO,
@@ -7,7 +7,7 @@ import {
   RestMessage
 } from '../model/whiteboard';
 import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
-import { share } from 'rxjs/operators';
+import { share, take } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class RestClientService implements OnInit {
@@ -22,52 +22,50 @@ export class RestClientService implements OnInit {
     this.headers.set('Content-Type', 'application/JSON');
   }
 
-  register(registrationDTO: RegistrationDTO): Observable<RestMessage<string>> {
-    return this.restClient('post', '/register', registrationDTO) as Observable<
+  register(registrationDTO: RegistrationDTO): Subject<RestMessage<string>> {
+    return this.restClient('post', '/register', registrationDTO) as Subject<
       RestMessage<string>
     >;
   }
 
-  login(loginDTO: LoginDTO): Observable<RestMessage<string>> {
-    return this.restClient('post', '/login', loginDTO) as Observable<
+  login(loginDTO: LoginDTO): Subject<RestMessage<string>> {
+    return this.restClient('post', '/login', loginDTO) as Subject<
       RestMessage<string>
     >;
   }
 
-  logout(username: string): Observable<RestMessage<string>> {
-    return this.restClient('post', '/logout', username) as Observable<
+  logout(username: string): Subject<RestMessage<string>> {
+    return this.restClient('post', '/logout', username) as Subject<
       RestMessage<string>
     >;
   }
 
-  create(itemDTO: ItemDTO): Observable<RestMessage<ItemDTO>> {
-    return this.restClient('post', '/create', itemDTO) as Observable<
+  create(itemDTO: ItemDTO): Subject<RestMessage<ItemDTO>> {
+    return this.restClient('post', '/create', itemDTO) as Subject<
       RestMessage<ItemDTO>
     >;
   }
 
-  load(): Observable<RestMessage<ItemDTO[]>> {
-    return this.restClient('get', '') as Observable<RestMessage<ItemDTO[]>>;
+  load(): Subject<RestMessage<ItemDTO[]>> {
+    return this.restClient('get', '') as Subject<RestMessage<ItemDTO[]>>;
   }
 
   restClient(
     method: string,
     path: string,
     data?: any
-  ): Observable<RestMessage<any>> {
+  ): Subject<RestMessage<any>> {
     path = `${this.api}${path}`;
     switch (method) {
       case 'get':
         return this.http
-          .get(path, { headers: this.headers })
-          .pipe(share()) as Observable<RestMessage<any>>;
+          .get(path, { headers: this.headers }) as Subject<RestMessage<any>>;
       default:
         if (data == null) {
           throwError('POST without data');
         }
         return this.http
-          .post(path, data, { headers: this.headers })
-          .pipe(share()) as Observable<RestMessage<any>>;
+          .post(path, data, { headers: this.headers }) as Subject<RestMessage<any>>;
     }
   }
 }
